c88f6ef86f68a5a3969331021f203692c581be43
Charlesdigital <charles.easton123@gmail.com>
2021-11-08 18:26:16 -0500

    add urls shortURL

--- express_server.js
+++ express_server.js
@@ -17,15 +17,20 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; //.longURL
+  res.render("urls_show", templateVars);
+});
