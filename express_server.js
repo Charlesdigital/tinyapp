de98a92df4f6006e2dad67eed40a22f39aa80076
Charlesdigital <charles.easton123@gmail.com>
2021-11-09 01:10:28 -0500

    add nodemon

--- express_server.js
+++ express_server.js
@@ -4,15 +4,17 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs"); // tells express app to use EJS
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const generateRandomString = function() {
-  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+const generateRandomString = function () {
+  return Math.floor((1 + Math.random()) * 0x1000000)
+    .toString(16)
+    .substring(1);
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -41,24 +43,27 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; //.longURL
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  }; //.longURL
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL]
-    if (longURL.startsWith("http")) {
-        res.redirect(longURL);
-      } else res.redirect("https://" + longURL);
-  });
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL.startsWith("http")) {
+    res.redirect(longURL);
+  } else res.redirect("https://" + longURL);
+});
 
 //---------------------------------------------------------> Post
 app.post("/urls", (req, res) => {
-const shortRandomURL = generateRandomString();
+  const shortRandomURL = generateRandomString();
 
-urlDatabase[shortRandomURL] = req.body.longURL
-console.log("test2", urlDatabase[shortRandomURL])
-console.log("tset 3",urlDatabase)
-  console.log("test1", req.body);  // Log the POST request body to the console
+  urlDatabase[shortRandomURL] = req.body.longURL;
+  console.log("test2", urlDatabase[shortRandomURL]);
+  console.log("test 3", urlDatabase);
+  console.log("test1", req.body); // Log the POST request body to the console
   res.redirect(`/urls/${shortRandomURL}`);
 });
